8e42ab75243f7e66cc93ad03ccbc81e1ebc1ae0f
Kristian Tot <kristiantot23@gmail.com>
2024-04-18 15:55:39 +0200

    move app.ts and mongoconnect.ts to /src folder

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import dbconnect from "./mongoconnect";
-import appsRoute from "./src/routes/applications";
-import githubAuth from "./src/routes/auth/github";
-import uiAuthRoute from "./src/routes/examples/auth";
+import dbconnect from "./mongoconnect.ts";
+import appsRoute from "./routes/applications.ts";
+import githubAuth from "./routes/auth/github.ts";
+import uiAuthRoute from "./routes/examples/auth.ts";
 
 await dbconnect();
 
