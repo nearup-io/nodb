b671965721c7b4ad13e0618fce0ea61d6af846c8
Kristian Tot <kristiantot23@gmail.com>
2024-06-11 08:13:44 +0200

    implement prisma transaction wrapper

--- src/repositories/postgres/base-repository.ts
+++ src/repositories/postgres/base-repository.ts
@@ -1,10 +1,15 @@
 import type { PrismaClient } from "@prisma/client";
+import type { ITXClientDenyList } from "@prisma/client/runtime/binary";
 
 abstract class BaseRepository {
   protected constructor(protected readonly prisma: PrismaClient) {}
 
-  protected async transaction<T>(callback: () => Promise<T>): Promise<T> {
-    return 1 as T;
+  protected async transaction<T>(
+    callback: (tx: Omit<PrismaClient, ITXClientDenyList>) => Promise<T>,
+  ): Promise<T> {
+    return this.prisma.$transaction(async (tx) => {
+      return callback(tx);
+    });
   }
 }
 
