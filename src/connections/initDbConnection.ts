628c5acd458a0fd1a2697b9aea87ee7c3a3f6a1e
Kristian Tot <kristiantot23@gmail.com>
2024-06-13 12:54:20 +0200

    update env variables

--- src/connections/initDbConnection.ts
+++ src/connections/initDbConnection.ts
@@ -3,7 +3,7 @@ import mongoConnect from "./mongodb.ts";
 import type { PrismaClient } from "@prisma/client";
 
 async function initDbConnection(): Promise<PrismaClient | undefined> {
-  if (Bun.env.database === "postgres") {
+  if (!!Bun.env.POSTGRES_URL) {
     return prisma;
   } else {
     await mongoConnect();
