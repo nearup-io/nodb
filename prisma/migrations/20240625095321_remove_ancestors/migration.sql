cb78b7f370327b235a929277c28fe91a368eb427
Kristian Tot <kristiantot23@gmail.com>
2024-06-25 11:58:52 +0200

    migration

---
+++ prisma/migrations/20240625095321_remove_ancestors/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ancestors` on the `Entity` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Entity" DROP COLUMN "ancestors";
