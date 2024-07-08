8b0f493473839a79a9f1745afa5639f284c2e550
Kristian Tot <kristiantot23@gmail.com>
2024-07-08 16:03:27 +0200

    add missing environment delete and post tests

--- tests/environment-crud/environment-delete.test.ts
+++ tests/environment-crud/environment-delete.test.ts
@@ -18,6 +18,95 @@ describe("DELETE /apps/:appName/:envName", async () => {
     await helper.stopApplication();
   });
 
+  describe("should return 401", async () => {
+    test("when no backend token or JWT token is provided", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key as string;
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}/dev`,
+      });
+      expect(deleteResponse.status).toBe(401);
+
+      const deleteAppResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteAppResponse.status).toBe(200);
+    });
+
+    test("when app token (backend token) does not have permissions for the application where it needs to delete the environment", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key as string;
+
+      const environmentName = "environment";
+      const environmentResponse = await helper.executePostRequest({
+        url: `/apps/${appName}/${environmentName}`,
+        backendToken: appToken,
+        body: {
+          description: "This is a staging environment",
+        },
+      });
+      expect(environmentResponse.status).toBe(201);
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}-1/${environmentName}`,
+        backendToken: appToken,
+      });
+
+      expect(deleteResponse.status).toBe(401);
+
+      const deleteAppResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteAppResponse.status).toBe(200);
+    });
+
+    test("when environment token (backend token) does not have permissions for the current environment that it's trying to delete", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key as string;
+      const envToken = appBody.environmentTokens[0].key as string;
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}/dev2`,
+        backendToken: envToken,
+      });
+
+      expect(deleteResponse.status).toBe(401);
+
+      const deleteAppResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteAppResponse.status).toBe(200);
+    });
+  });
+
   test("should return 200 OK and found false when environment is not found", async () => {
     const response = await helper.executePostRequest({
       url: `/apps/${appName}`,
@@ -44,7 +133,7 @@ describe("DELETE /apps/:appName/:envName", async () => {
     expect(deleteAppResponse.status).toBe(200);
   });
 
-  test("should return 200 OK and found true when environment is deleted successfully", async () => {
+  test("should return 200 OK and found true when environment is deleted successfully with jwt auth", async () => {
     const response = await helper.executePostRequest({
       url: `/apps/${appName}`,
       jwtToken: jwtToken,
@@ -79,6 +168,73 @@ describe("DELETE /apps/:appName/:envName", async () => {
     const deleteAppResponse = await helper.executeDeleteRequest({
       url: `/apps/${appName}`,
       jwtToken: jwtToken,
+    });
+    expect(deleteAppResponse.status).toBe(200);
+  });
+
+  test("should return 200 OK and found true when environment is deleted successfully with application token (backend token)", async () => {
+    const environmentName = "environment";
+
+    const postAppResponse = await helper.executePostRequest({
+      url: `/apps/${appName}`,
+      body: {
+        image: "path/to/image.jpg",
+        description: "Memes app",
+        environmentName,
+      },
+    });
+    expect(postAppResponse.status).toBe(201);
+    const appBody = await postAppResponse.json();
+    const appToken = appBody.applicationTokens[0].key as string;
+    const deleteResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}/${environmentName}`,
+      backendToken: appToken,
+    });
+
+    expect(deleteResponse.status).toBe(200);
+    expect(await deleteResponse.json()).toEqual({ found: true });
+    expect(await helper.getEnvironmentFromDbByName(environmentName)).toBeNull();
+
+    const environmentsByApp = await helper.getEnvironmentsFromAppName(appName);
+    expect(environmentsByApp).not.toContain(environmentName);
+
+    const deleteAppResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}`,
+      backendToken: appToken,
+    });
+    expect(deleteAppResponse.status).toBe(200);
+  });
+
+  test("should return 200 OK and found true when environment is deleted successfully with environment token (backend token)", async () => {
+    const environmentName = "environment";
+
+    const postAppResponse = await helper.executePostRequest({
+      url: `/apps/${appName}`,
+      body: {
+        image: "path/to/image.jpg",
+        description: "Memes app",
+        environmentName,
+      },
+    });
+    expect(postAppResponse.status).toBe(201);
+    const appBody = await postAppResponse.json();
+    const appToken = appBody.applicationTokens[0].key as string;
+    const envToken = appBody.environmentTokens[0].key as string;
+    const deleteResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}/${environmentName}`,
+      backendToken: envToken,
+    });
+
+    expect(deleteResponse.status).toBe(200);
+    expect(await deleteResponse.json()).toEqual({ found: true });
+    expect(await helper.getEnvironmentFromDbByName(environmentName)).toBeNull();
+
+    const environmentsByApp = await helper.getEnvironmentsFromAppName(appName);
+    expect(environmentsByApp).not.toContain(environmentName);
+
+    const deleteAppResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}`,
+      backendToken: appToken,
     });
     expect(deleteAppResponse.status).toBe(200);
   });
--- tests/environment-crud/environment-post.test.ts
+++ tests/environment-crud/environment-post.test.ts
@@ -60,7 +60,100 @@ describe("POST /apps/:appName/:envName", async () => {
     expect(deleteResponse.status).toBe(200);
   });
 
-  test("should return 201 CREATED and create the environment for the app", async () => {
+  describe("should return 401", async () => {
+    test("when no backend token or JWT token is provided", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        jwtToken: jwtToken,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key as string;
+      // first environment
+      const environmentName = "environment";
+      const firstEnvironmentResponse = await helper.executePostRequest({
+        url: `/apps/${appName}/${environmentName}`,
+        body: {
+          description: "This is a staging environment",
+        },
+      });
+      expect(firstEnvironmentResponse.status).toBe(401);
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteResponse.status).toBe(200);
+    });
+
+    test("when app token (backend token) does not have permissions for the application where it wants to add a new environment", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        jwtToken: jwtToken,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key;
+      // first environment
+      const environmentName = "environment";
+      const firstEnvironmentResponse = await helper.executePostRequest({
+        url: `/apps/${appName}-2/${environmentName}`,
+        backendToken: appToken,
+        body: {
+          description: "This is a staging environment",
+        },
+      });
+      expect(firstEnvironmentResponse.status).toBe(401);
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteResponse.status).toBe(200);
+    });
+
+    test("when environment token (backend token) does not have permissions for the current app it's trying to add an environment to", async () => {
+      const postAppResponse = await helper.executePostRequest({
+        url: `/apps/${appName}`,
+        jwtToken: jwtToken,
+        body: {
+          image: "path/to/image.jpg",
+          description: "Memes app",
+        },
+      });
+      expect(postAppResponse.status).toBe(201);
+      const appBody = await postAppResponse.json();
+      const appToken = appBody.applicationTokens[0].key as string;
+      const envToken = appBody.applicationTokens[0].key as string;
+      // first environment
+      const environmentName = "environment";
+      const firstEnvironmentResponse = await helper.executePostRequest({
+        url: `/apps/${appName}-2/${environmentName}`,
+        backendToken: envToken,
+        body: {
+          description: "This is a staging environment",
+        },
+      });
+      expect(firstEnvironmentResponse.status).toBe(401);
+
+      const deleteResponse = await helper.executeDeleteRequest({
+        url: `/apps/${appName}`,
+        backendToken: appToken,
+      });
+      expect(deleteResponse.status).toBe(200);
+    });
+  });
+
+  test("should return 201 CREATED and create the environment for the app using JWT auth", async () => {
     const response = await helper.executePostRequest({
       url: `/apps/${appName}`,
       jwtToken: jwtToken,
@@ -111,6 +204,121 @@ describe("POST /apps/:appName/:envName", async () => {
     const deleteResponse = await helper.executeDeleteRequest({
       url: `/apps/${appName}`,
       jwtToken: jwtToken,
+    });
+    expect(deleteResponse.status).toBe(200);
+  });
+
+  test("should return 201 CREATED and create the environment for the app using application token (backend token) auth", async () => {
+    const postAppResponse = await helper.executePostRequest({
+      url: `/apps/${appName}`,
+      body: {
+        image: "path/to/image.jpg",
+        description: "Memes app",
+      },
+    });
+    expect(postAppResponse.status).toBe(201);
+    const appBody = await postAppResponse.json();
+    const appToken = appBody.applicationTokens[0].key as string;
+
+    const environmentName = "environment-1";
+    const environmentResponse = await helper.executePostRequest({
+      url: `/apps/${appName}/${environmentName}`,
+      backendToken: appToken,
+      body: {
+        description: "This is a staging environment",
+      },
+    });
+    expect(environmentResponse.status).toBe(201);
+
+    const environment = (await helper.getEnvironmentFromDbByName(
+      environmentName,
+    )) as Omit<EnvironmentType, "app"> | null;
+    expect(environment).not.toBeNull();
+    expect(R.keys(environment!)).toEqual([
+      "id",
+      "name",
+      "tokens",
+      "entities",
+      "description",
+    ]);
+
+    const { id, tokens, ...props } = environment!;
+    expect(id).toBeDefined();
+    expect(tokens).toBeArray();
+    const [firstToken] = tokens;
+    expect(firstToken.key).toBeString();
+    expect(firstToken.permission).toBeString();
+
+    expect(props).toEqual({
+      description: "This is a staging environment",
+      entities: [],
+      name: environmentName,
+    });
+
+    const environments = await helper.getEnvironmentsFromAppName(appName);
+    expect(environments).toContain(environmentName);
+
+    const deleteResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}`,
+      backendToken: appToken,
+    });
+    expect(deleteResponse.status).toBe(200);
+  });
+
+  test("should return 201 CREATED and create the environment for the app using environment token (backend token) auth", async () => {
+    const postAppResponse = await helper.executePostRequest({
+      url: `/apps/${appName}`,
+      body: {
+        image: "path/to/image.jpg",
+        description: "Memes app",
+      },
+    });
+    expect(postAppResponse.status).toBe(201);
+    const appBody = await postAppResponse.json();
+    const appToken = appBody.applicationTokens[0].key as string;
+    const envToken = appBody.environmentTokens[0].key as string;
+
+    const environmentName = "environment-1";
+    const environmentResponse = await helper.executePostRequest({
+      url: `/apps/${appName}/${environmentName}`,
+      backendToken: envToken,
+      body: {
+        description: "This is a staging environment",
+      },
+    });
+    expect(environmentResponse.status).toBe(201);
+
+    const environment = (await helper.getEnvironmentFromDbByName(
+      environmentName,
+    )) as Omit<EnvironmentType, "app"> | null;
+    expect(environment).not.toBeNull();
+    expect(R.keys(environment!)).toEqual([
+      "id",
+      "name",
+      "tokens",
+      "entities",
+      "description",
+    ]);
+
+    const { id, tokens, ...props } = environment!;
+    expect(id).toBeDefined();
+    expect(tokens).toBeArray();
+    const [firstToken] = tokens;
+    expect(firstToken.key).toBeString();
+    expect(firstToken.permission).toBeString();
+
+    expect(props).toEqual({
+      description: "This is a staging environment",
+      entities: [],
+      name: environmentName,
+    });
+
+    const environments = await helper.getEnvironmentsFromAppName(appName);
+    expect(environments).toContain(environmentName);
+
+    const deleteResponse = await helper.executeDeleteRequest({
+      url: `/apps/${appName}`,
+      backendToken: appToken,
     });
     expect(deleteResponse.status).toBe(200);
   });
