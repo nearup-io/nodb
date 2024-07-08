c8ee41c3b35b2cd3384e9ca9311a60e88e08ce21
Kristian Tot <kristiantot23@gmail.com>
2024-07-08 09:33:22 +0200

    fix ts issues

--- src/routes/entities.ts
+++ src/routes/entities.ts
@@ -15,11 +15,7 @@ import { httpError } from "../utils/const";
 import { asyncTryJson } from "../utils/route-utils";
 import { entityQueryValidator } from "../utils/route-validators";
 import { RoutingError, ServiceError } from "../utils/service-errors";
-import type {
-  EntityRequestDto,
-  EntityRouteParams,
-  PostEntityRequestDto,
-} from "../utils/types.ts";
+import type { EntityRequestDto, PostEntityRequestDto } from "../utils/types.ts";
 import type Context from "../utils/context.ts";
 import { flexibleAuthMiddleware } from "../middlewares";
 
@@ -34,196 +30,174 @@ const app = new Hono<
   "/:appName/:envName/:entityName"
 >();
 
-app.get(
-  "/",
-  entityQueryValidator(),
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  async (c) => {
-    const q = c.req.valid("query");
-    const context = c.get("context");
+app.use(flexibleAuthMiddleware({ allowBackendToken: true }));
 
-    try {
-      const result = await getEntities({
-        context,
-        propFilters: q.props,
-        metaFilters: q.meta,
-        routeParams: c.req.param() as EntityRouteParams,
-        rawQuery: c.req.query(),
-      });
-      return c.json(result);
-    } catch (error) {
-      if (error instanceof ServiceError) {
-        if (
-          [httpError.ENV_DOESNT_EXIST, httpError.ENTITY_NOT_FOUND].includes(
-            error.explicitMessage,
-          )
-        ) {
-          throw new HTTPException(404, {
-            message: error.explicitMessage,
-          });
-        } else {
-          throw new HTTPException(400, {
-            message: error.explicitMessage,
-          });
-        }
-      } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
+app.get("/", entityQueryValidator(), async (c) => {
+  const q = c.req.valid("query");
+  const context = c.get("context");
+
+  try {
+    const result = await getEntities({
+      context,
+      propFilters: q.props,
+      metaFilters: q.meta,
+      routeParams: c.req.param(),
+      rawQuery: c.req.query(),
+    });
+    return c.json(result);
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      if (
+        [httpError.ENV_DOESNT_EXIST, httpError.ENTITY_NOT_FOUND].includes(
+          error.explicitMessage,
+        )
+      ) {
+        throw new HTTPException(404, {
+          message: error.explicitMessage,
         });
-      }
-    }
-  },
-);
-
-app.get(
-  "/:entityId",
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  entityQueryValidator(),
-  async (c) => {
-    const q = c.req.valid("query");
-    const context = c.get("context");
-
-    try {
-      const entity = await getSingleEntity({
-        xpath: c.req.path,
-        context,
-        requestParams: c.req.param() as EntityRouteParams & {
-          entityId: string;
-        },
-        metaFilters: q.meta,
-      });
-      return c.json(entity);
-    } catch (error) {
-      if (error instanceof ServiceError) {
-        if (
-          [httpError.ENV_DOESNT_EXIST, httpError.ENTITY_NOT_FOUND].includes(
-            error.explicitMessage,
-          )
-        ) {
-          throw new HTTPException(404, {
-            message: error.explicitMessage,
-          });
-        } else {
-          throw new HTTPException(400, {
-            message: error.explicitMessage,
-          });
-        }
       } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
-        });
-      }
-    }
-  },
-);
-
-app.post(
-  "/",
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  async (c) => {
-    const { appName, envName, entityName } = c.req.param();
-
-    const body = (await asyncTryJson(c.req.json())) as PostEntityRequestDto[];
-    if (!Array.isArray(body)) {
-      throw new HTTPException(400, {
-        message: httpError.BODY_IS_NOT_ARRAY,
-      });
-    }
-    try {
-      const ids = await createOrOverwriteEntities({
-        context: c.get("context"),
-        appName,
-        envName,
-        entityName,
-        bodyEntities: body,
-      });
-      c.status(201);
-      return c.json({ ids });
-    } catch (e) {
-      if (e instanceof ServiceError || e instanceof RoutingError) {
         throw new HTTPException(400, {
-          message: e.explicitMessage,
-        });
-      } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
+          message: error.explicitMessage,
         });
       }
-    }
-  },
-);
-
-app.delete(
-  "/",
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  async (c) => {
-    const { appName, envName, entityName } = c.req.param();
-    const context = c.get("context");
-    try {
-      const res = await deleteRootAndUpdateEnv({
-        context,
-        appName,
-        envName,
-        entityName,
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
       });
-      return c.json({ deleted: res.done });
-    } catch (error) {
-      if (error instanceof ServiceError) {
-        if (error.explicitMessage === httpError.ENV_DOESNT_EXIST) {
-          throw new HTTPException(404, {
-            message: error.explicitMessage,
-          });
-        } else {
-          throw new HTTPException(400, {
-            message: error.explicitMessage,
-          });
-        }
+    }
+  }
+});
+
+app.get("/:entityId", entityQueryValidator(), async (c) => {
+  const q = c.req.valid("query");
+  const context = c.get("context");
+  c.req.param();
+  try {
+    const entity = await getSingleEntity({
+      xpath: c.req.path,
+      context,
+      requestParams: c.req.param(),
+      metaFilters: q.meta,
+    });
+    return c.json(entity);
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      if (
+        [httpError.ENV_DOESNT_EXIST, httpError.ENTITY_NOT_FOUND].includes(
+          error.explicitMessage,
+        )
+      ) {
+        throw new HTTPException(404, {
+          message: error.explicitMessage,
+        });
       } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
+        throw new HTTPException(400, {
+          message: error.explicitMessage,
         });
       }
-    }
-  },
-);
-
-app.delete(
-  "/:entityId",
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  async (c) => {
-    const { appName, envName, entityName, entityId } = c.req.param();
-    const context = c.get("context");
-    try {
-      const res = await deleteSingleEntityAndUpdateEnv({
-        context,
-        appName,
-        envName,
-        entityName,
-        entityId,
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
       });
+    }
+  }
+});
 
-      return c.json({ deleted: !!res });
-    } catch (error) {
-      if (error instanceof ServiceError) {
-        if (error.explicitMessage === httpError.ENV_DOESNT_EXIST) {
-          throw new HTTPException(404, {
-            message: error.explicitMessage,
-          });
-        } else {
-          throw new HTTPException(400, {
-            message: error.explicitMessage,
-          });
-        }
+app.post("/", async (c) => {
+  const { appName, envName, entityName } = c.req.param();
+
+  const body = (await asyncTryJson(c.req.json())) as PostEntityRequestDto[];
+  if (!Array.isArray(body)) {
+    throw new HTTPException(400, {
+      message: httpError.BODY_IS_NOT_ARRAY,
+    });
+  }
+  try {
+    const ids = await createOrOverwriteEntities({
+      context: c.get("context"),
+      appName,
+      envName,
+      entityName,
+      bodyEntities: body,
+    });
+    c.status(201);
+    return c.json({ ids });
+  } catch (e) {
+    if (e instanceof ServiceError || e instanceof RoutingError) {
+      throw new HTTPException(400, {
+        message: e.explicitMessage,
+      });
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
+      });
+    }
+  }
+});
+
+app.delete("/", async (c) => {
+  const { appName, envName, entityName } = c.req.param();
+  const context = c.get("context");
+  try {
+    const res = await deleteRootAndUpdateEnv({
+      context,
+      appName,
+      envName,
+      entityName,
+    });
+    return c.json({ deleted: res.done });
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      if (error.explicitMessage === httpError.ENV_DOESNT_EXIST) {
+        throw new HTTPException(404, {
+          message: error.explicitMessage,
+        });
       } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
+        throw new HTTPException(400, {
+          message: error.explicitMessage,
         });
       }
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
+      });
     }
-  },
-);
+  }
+});
 
-app.put("/", flexibleAuthMiddleware({ allowBackendToken: true }), async (c) => {
+app.delete("/:entityId", async (c) => {
+  const { appName, envName, entityName, entityId } = c.req.param();
+  const context = c.get("context");
+  try {
+    const res = await deleteSingleEntityAndUpdateEnv({
+      context,
+      appName,
+      envName,
+      entityName,
+      entityId,
+    });
+
+    return c.json({ deleted: !!res });
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      if (error.explicitMessage === httpError.ENV_DOESNT_EXIST) {
+        throw new HTTPException(404, {
+          message: error.explicitMessage,
+        });
+      } else {
+        throw new HTTPException(400, {
+          message: error.explicitMessage,
+        });
+      }
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
+      });
+    }
+  }
+});
+
+app.put("/", async (c) => {
   const { appName, envName, entityName } = c.req.param();
 
   const bodyEntities = await asyncTryJson<EntityRequestDto[]>(c.req.json());
@@ -255,39 +229,35 @@ app.put("/", flexibleAuthMiddleware({ allowBackendToken: true }), async (c) => {
   }
 });
 
-app.patch(
-  "/",
-  flexibleAuthMiddleware({ allowBackendToken: true }),
-  async (c) => {
-    const { appName, envName, entityName } = c.req.param();
-    const bodyEntities = await asyncTryJson<EntityRequestDto[]>(c.req.json());
-    if (!Array.isArray(bodyEntities)) {
-      throw new HTTPException(400, {
-        message: httpError.BODY_IS_NOT_ARRAY,
-      });
-    }
-    try {
-      const ids = await updateEntities({
-        context: c.get("context"),
-        appName,
-        envName,
-        entityName,
-        bodyEntities,
-      });
+app.patch("/", async (c) => {
+  const { appName, envName, entityName } = c.req.param();
+  const bodyEntities = await asyncTryJson<EntityRequestDto[]>(c.req.json());
+  if (!Array.isArray(bodyEntities)) {
+    throw new HTTPException(400, {
+      message: httpError.BODY_IS_NOT_ARRAY,
+    });
+  }
+  try {
+    const ids = await updateEntities({
+      context: c.get("context"),
+      appName,
+      envName,
+      entityName,
+      bodyEntities,
+    });
 
-      return c.json({ ids });
-    } catch (e) {
-      if (e instanceof ServiceError) {
-        throw new HTTPException(400, {
-          message: e.explicitMessage,
-        });
-      } else {
-        throw new HTTPException(500, {
-          message: httpError.UNKNOWN,
-        });
-      }
+    return c.json({ ids });
+  } catch (e) {
+    if (e instanceof ServiceError) {
+      throw new HTTPException(400, {
+        message: e.explicitMessage,
+      });
+    } else {
+      throw new HTTPException(500, {
+        message: httpError.UNKNOWN,
+      });
     }
-  },
-);
+  }
+});
 
 export default app;
