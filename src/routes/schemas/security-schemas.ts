110edfaa04db4698a9de237c502383ee314fe070
Kristian Tot <kristiantot23@gmail.com>
2024-07-12 10:58:21 +0200

    update security schema

--- src/routes/schemas/security-schemas.ts
+++ src/routes/schemas/security-schemas.ts
@@ -1,18 +1,15 @@
 import { z } from "zod";
 
-const ClerkJwtSchema = z.object({
-  jwt: z.string().optional(),
-});
-
-const BackendTokenSchema = z.object({
-  token: z.string().optional(),
-});
-
 export const SecuritySchema = z
   .object({
-    jwt: z.string().optional(),
+    authorization: z.string().optional(),
     token: z.string().optional(),
   })
-  .refine((data) => data["jwt"] !== undefined || data["token"] !== undefined, {
-    message: "Either jwt or token must be provided",
-  });
+  .refine(
+    (data) => {
+      return data["authorization"] !== undefined || data["token"] !== undefined;
+    },
+    {
+      message: "Either authorization header or token header must be provided",
+    },
+  );
