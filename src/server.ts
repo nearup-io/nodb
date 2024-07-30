9ce232718c7cb16cbf263ff50ccbfedadf50e2fb
Marko Jakic <jakicmeister@gmail.com>
2024-07-30 12:08:33 +0200

    .dockerignore and necessary clerk check

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import { logger } from "hono/logger";
-import initDbConnection from "./connections/initDbConnection.ts";
-import { cors } from "hono/cors";
 import { clerkMiddleware } from "@hono/clerk-auth";
-import { contextMiddleware } from "./middlewares";
-import usersRoute from "./routes/users.ts";
-import appsRoute from "./routes/applications.ts";
-import searchRoute from "./routes/search.ts";
-import ragRoute from "./routes/rag.ts";
-import tokenRoute from "./routes/tokens.ts";
-import errorHandler from "./middlewares/error-handler.middleware.ts";
 import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import initDbConnection from "./connections/initDbConnection.ts";
+import { contextMiddleware } from "./middlewares";
+import errorHandler from "./middlewares/error-handler.middleware.ts";
+import appsRoute from "./routes/applications.ts";
+import ragRoute from "./routes/rag.ts";
+import searchRoute from "./routes/search.ts";
+import tokenRoute from "./routes/tokens.ts";
+import usersRoute from "./routes/users.ts";
 import wsRoutes from "./routes/websockets.ts";
 
 export const startApp = async (props?: {
@@ -33,9 +33,11 @@ export const startApp = async (props?: {
     cors({
       origin: ["http://localhost:5173"],
       credentials: true,
-    }),
+    })
   );
-  app.use("*", clerkMiddleware());
+  if (Bun.env.CLERK_SECRET_KEY && Bun.env.CLERK_PUBLISHABLE_KEY) {
+    app.use("*", clerkMiddleware());
+  }
   app.use(contextMiddleware(db));
   app.get("/swagger", swaggerUI({ url: "/doc" }));
   app.route("/users", usersRoute);
